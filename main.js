210c2632ae0b82ac3282921a769bae29346ec80a
Nyll0 <pmurawski69@gmail.com>
2023-12-19 15:11:07 +0100

    Added function game to play a one round game with the computer

--- main.js
+++ main.js
@@ -15,3 +15,41 @@ function getComputerChoice(){
 };
 
 console.log(getComputerChoice());
+
+let playerSelection = prompt("chose your move!");
+
+playerSelection = playerSelection.toLowerCase();
+
+let computerSelection = getComputerChoice();
+
+function game(playerSelection,computerSelection){
+  if(playerSelection === computerSelection){
+    return 'The game is a tie !';
+  }
+  if(playerSelection === 'rock'){
+    if(computerSelection === 'paper'){
+      return 'The computer won !';
+    }
+    else{
+      return 'You won !';
+    }
+  }
+
+  if(playerSelection === 'paper'){
+    if(computerSelection === 'scissors'){
+      return 'The computer won!';
+    }
+    else{
+      return 'You won!'
+    }
+  }
+
+  if(playerSelection === 'scissors'){
+    if(computerSelection === 'rock'){
+      return 'The computer won!';
+    }
+    else{
+      return 'You won!'
+    }
+  }
+};
